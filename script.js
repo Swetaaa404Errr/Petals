e0a2f737aba910d19325f4cf04373fc32860c5f5
Sweta <swetadas1099@gmail.com>
2022-04-12 12:22:34 +0530

    Initia commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let navLinks = document.getElementById("navlinks");
+let menu = document.getElementById("menu");
+
+$("#menu").on("click", function () {
+  $(".navlinks__transition").toggleClass("navlinks__transition__toggle");
+});
